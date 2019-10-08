4fdb555e522183fcda2dfde88b6fd1199da8b545
ninal1811 <56265101+ninal1811@users.noreply.github.com>
2019-10-08 11:02:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Nina Li"
+let userName = "ninal1811";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
